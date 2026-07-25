8a2cfe016b5c1518417d95639a1d65c1696d08f9
togrulcorporation-cyber <togrulcorporation@gmail.com>
2026-07-26 03:10:59 +0400

    Remove service worker event listeners
    Remove service worker event listeners for install, activate, and fetch.

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,1 @@
-self.addEventListener("install", function(event) {
-  self.skipWaiting();
-});
 
-self.addEventListener("activate", function(event) {
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", function(event) {
-  event.respondWith(fetch(event.request));
-});
