85a37e94dad9458f3843e8742d3ba06554796433
togrulcorporation-cyber <togrulcorporation@gmail.com>
2026-07-31 13:22:15 +0400

    Update core.js

--- js/core.js
+++ js/core.js
@@ -9,6 +9,7 @@ var currentUser = null;
 var SESSION_KEY = "ctech_session";
 var clockStarted = false;
 var notifPollingStarted = false;
+var dashStatsPollingStarted = false;
 var _dotVisible = true;
 var MOON_PATH = '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>';
 var SUN_PATH = '<circle cx="12" cy="12" r="4"/><path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M4.93 19.07l1.41-1.41M17.66 6.34l1.41-1.41"/>';
@@ -171,6 +172,12 @@ function showDashboard(){
     setInterval(function(){
       if(typeof preloadNotifications==='function') preloadNotifications();
     }, 120000);
+  }
+  if(!dashStatsPollingStarted){
+    dashStatsPollingStarted = true;
+    setInterval(function(){
+      if(typeof loadHomeDashStats==='function') loadHomeDashStats();
+    }, 180000);
   }
   if(!clockStarted){ clockStarted=true; updateClock(); setInterval(updateClock,1000); }
 }
